f7689fcb2fda3c8ed210739b9790767334a9e3d0
AitorRD <aitroddue@alum.us.es>
2024-05-13 14:07:02 +0200

    feat(tsfresh): tsfresh added + refactor

--- src/model/knn.py
+++
@@ -1,8 +0,0 @@
-from sklearn.neighbors import KNeighborsRegressor
-
-def train_knn(X_train_list, y_train_list, k):
-    knn = KNeighborsRegressor(n_neighbors=k)
-    for X_train, y_train in zip(X_train_list, y_train_list):
-        knn.fit(X_train, y_train)
-    print('KNN Trained')
-    return knn
