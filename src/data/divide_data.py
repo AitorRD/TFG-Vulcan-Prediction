7ac3da1bab3bd05c8be78712e3a86880d7b2a797
AitorRD <aitroddue@alum.us.es>
2024-05-13 14:19:30 +0200

    feat(tt): added tt divide data from previous metrics

--- src/data/divide_data.py
+++ src/data/divide_data.py
@@ -1,6 +1,32 @@
 import pandas as pd
-from sklearn.model_selection import TimeSeriesSplit, KFold
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
+
+def divide_data_tt(data_file):
+    df = pd.read_csv(data_file)
+    X = df.drop(columns=['time_to_eruption', 'volcan_id'])
+    y = df['time_to_eruption']
+
+    imputer = SimpleImputer(strategy='median')
+    X_imputed = imputer.fit_transform(X)
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X_imputed)
+
+
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+    X_train_list = []
+    X_test_list = []
+    y_train_list = []
+    y_test_list = []
+
+    X_train_list.append(X_train)
+    X_test_list.append(X_test)
+    y_train_list.append(y_train)
+    y_test_list.append(y_test)
+
+    return X_train_list, X_test_list, y_train_list, y_test_list, X_imputed
 
 def divide_data_kfold(data_file, n_splits=5):
     df = pd.read_csv(data_file)
@@ -18,32 +44,6 @@ def divide_data_kfold(data_file, n_splits=5):
     y_test_list = []
 
     for train_index, test_index in kf.split(X):
-        X_train, X_test = X_imputed[train_index], X_imputed[test_index]
-        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-        X_train_list.append(X_train)
-        X_test_list.append(X_test)
-        y_train_list.append(y_train)
-        y_test_list.append(y_test)
-
-    return X_train_list, X_test_list, y_train_list, y_test_list, X_imputed
-
-
-def divide_data_time_series(data_file, n_splits=5):
-    df = pd.read_csv(data_file)
-    X = df.drop(columns=['time_to_eruption', 'volcan_id'])
-    y = df['time_to_eruption']
-
-    imputer = SimpleImputer(strategy='median')
-    X_imputed = imputer.fit_transform(X)
-
-    tscv = TimeSeriesSplit(n_splits=n_splits)
-
-    X_train_list = []
-    X_test_list = []
-    y_train_list = []
-    y_test_list = []
-
-    for train_index, test_index in tscv.split(X):
         X_train, X_test = X_imputed[train_index], X_imputed[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
         X_train_list.append(X_train)
