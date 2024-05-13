eb09502d521aa2f4424be0724cb4483133c252a4
AitorRD <aitroddue@alum.us.es>
2024-05-13 18:42:19 +0200

    fix(tsfresh): new algorithm to process the data

--- src/tsfresh/feature_extractor.py
+++ src/tsfresh/feature_extractor.py
@@ -1,5 +1,6 @@
 from tsfresh import extract_features, extract_relevant_features
 from tsfresh.feature_extraction import MinimalFCParameters
+from tsfresh.utilities.dataframe_functions import impute
 from sklearn import feature_selection, feature_extraction #TODO hacer esto tmb 
 import pandas as pd
 import os
@@ -7,9 +8,9 @@ import time
 
 class FeatureExtractor:
     def __init__(self):
-        pass
+        self.counter = 1
 
-    def extract_features(self, directory, use_relevant_features=False):
+    def extract_features(self, directory):
         start_time = time.time()
         extracted_features = []
 
@@ -17,25 +18,24 @@ class FeatureExtractor:
             if filename.endswith(".csv"):
                 filepath = os.path.join(directory, filename)
                 time_series_data = pd.read_csv(filepath) 
-                time_series_data['id'] = range(1, len(time_series_data) + 1)
-                time_series_data = self.handle_nan_values(time_series_data)
-                if use_relevant_features:
-                    relevant_features = extract_relevant_features(time_series_data, column_id='id')
-                    extracted_features.append(relevant_features)
-                else:
-                    features = extract_features(time_series_data, column_id='id', default_fc_parameters=MinimalFCParameters())
-                    extracted_features.append(features)
+                time_series_data.insert(0, 'time', range(1, len(time_series_data) + 1))
+                time_series_data.insert(1, 'id', self.counter)
+                self.counter += 1
+                time_series_data = self.procesar_sensores(time_series_data)
+                
+                features = extract_features(time_series_data, column_id='id', column_sort='time', default_fc_parameters=MinimalFCParameters())
+                impute(features)
+
+                volcan_id = os.path.splitext(filename)[0]
+                features.insert(0, 'volcan_id', volcan_id)
+                extracted_features.append(features)
 
         end_time = time.time()
         execution_time = end_time - start_time
-        print(f"Extracción de características completada en {execution_time} segundos")
+        print(f"Extracción de características completada en {execution_time/60} minutos")
 
-        if use_relevant_features:
-            filename = "tsfresh_processed_relevant_feat.csv"
-        else:
-            filename = "tsfresh_processed_minfc_feat.csv"
-
-        output_dir = "src/data/processed"
+        filename = "tsfresh_data_minfc.csv"
+        output_dir = "processed"
         os.makedirs(output_dir, exist_ok=True)
         filepath = os.path.join(output_dir, filename)
         df = pd.concat(extracted_features)
@@ -44,21 +44,14 @@ class FeatureExtractor:
 
         return extracted_features
     
-    def handle_nan_values(self, time_series_data):
-        # Eliminar columnas completas con valores NaN
-        time_series_data.dropna(axis=1, how='all', inplace=True)
-
-        # Calcular la media de cada columna
-        column_means = time_series_data.mean(axis=0)
-
-        # Imputar la media a los valores NaN de cada columna
-        time_series_data.fillna(column_means, inplace=True)
-
+    def procesar_sensores(self, time_series_data):
+        time_series_data['media_sensor'] = time_series_data.mean(axis=1)
+        time_series_data = time_series_data[['id','time','media_sensor']]
         return time_series_data
 
 if __name__ == '__main__':
     feature_extractor = FeatureExtractor()
-    directory = "../data/kaggle/input/train"
-    extracted_features = feature_extractor.extract_features(directory, False)
+    directory = "../data/prueba"
+    extracted_features = feature_extractor.extract_features(directory)
 
 #TODO MLP (Neuronal), ADABOOST y XGBoost mirarmelo
