e1093b8b5dd4a605e63d9a196354d9d892bc4a2d
AitorRD <aitroddue@alum.us.es>
2024-06-25 18:37:07 +0200

    chore(rename): rename csv

--- src/data/mapbox/script_coordinates.py
+++ src/data/mapbox/script_coordinates.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Leer los datos de los archivos CSV
 datos_volcanes = pd.read_csv('volcanes_clean.csv')
-datos_tiempo_erupcion = pd.read_csv('results_gboost_kfold_default.csv')
+datos_tiempo_erupcion = pd.read_csv('results.csv')
 
 # Fusionar los datos utilizando el método merge de Pandas
 volcanes_con_tiempo_erupcion = pd.concat([datos_volcanes, datos_tiempo_erupcion['time_to_eruption_dt']], axis=1)
